6eaa99552980196bc745aca739453fb03f334ae9
Mateusz Żarkowski <zarkowski.mateusz@gmail.com>
2024-02-20 10:23:59 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-pip install b2
 B2_BUCKET_NAME=runpod-storage-xyz
 
-b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/ComfyUI/custom_nodes" "/workspace/ComfyUI/custom_nodes"
-b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/ComfyUI/models" "/workspace/ComfyUI/models"
-b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/storage/stable_diffusion/models/ckpt" "/workspace/ComfyUI/models/checkpoints"
-b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/storage/stable_diffusion/models/controlnet" "/workspace/ComfyUI/models/controlnet"
-b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/storage/stable_diffusion/models/esrgan" "/workspace/ComfyUI/models/upscale_models"
-b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/ComfyUI/input" "/workspace/ComfyUI/input"
+/opt/micromamba/envs/comfyui/bin/b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/ComfyUI/custom_nodes" "/workspace/ComfyUI/custom_nodes"
+/opt/micromamba/envs/comfyui/bin/b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/ComfyUI/models" "/workspace/ComfyUI/models"
+/opt/micromamba/envs/comfyui/bin/b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/storage/stable_diffusion/models/ckpt" "/workspace/ComfyUI/models/checkpoints"
+/opt/micromamba/envs/comfyui/bin/b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/storage/stable_diffusion/models/controlnet" "/workspace/ComfyUI/models/controlnet"
+/opt/micromamba/envs/comfyui/bin/b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/storage/stable_diffusion/models/esrgan" "/workspace/ComfyUI/models/upscale_models"
+/opt/micromamba/envs/comfyui/bin/b2 sync --skipNewer "b2://${B2_BUCKET_NAME}/final2/workspace/ComfyUI/input" "/workspace/ComfyUI/input"
